10ad021d5b814f0aa602817a08d4534c6541e704
“miloferrero17” <milonguitaferrero@gmail.com>
2025-01-11 09:02:35 -0300

    Descripción de los cambios realizados

--- app/routes.py
+++ app/routes.py
@@ -31,7 +31,7 @@ def handle_post_request():
         conversation_history.append({
             "role": "user",
             "content":  "Crees que en el siguiente mensaje: " + mensaje +
-            " contiene respuestas de las siguiente informacion: 1. Nombre y Apellido del invitado ||| 2. Preferencia Alimentaria siendo: 1 - celiaco; 2 - diabetico; 3 - vegetariano; 4 - vegano; 5 - Sin preferencia ||| 3. Si confirma a alguien más ||| contestame únicamente un texto con (en este orden): + número de pregunta este la informacion o no (1,2 o 3) // + '1' si estás 100% seguro que con la respuesta del usuario se le puede dar una respuesta a esa pregunta o '0' // + y que estes seguro de la respuesta cual es || ejemplo de output: 1, Si, Emilio Ferrero; 2, No, NA; 3, No, NA;"
+            " contiene respuestas de las siguiente informacion: 1. Nombre y Apellido del invitado ||| 2. Preferencia Alimentaria siendo: 1 - celiaco; 2 - diabetico; 3 - vegetariano; 4 - vegano; 5 - Sin preferencia ||| 3. Si confirma a alguien más ||| contestame únicamente un texto con (en este orden): + número de pregunta este la informacion o no (1,2 o 3) // + '1' si estás 100% seguro que con la respuesta del usuario se le puede dar una respuesta a esa pregunta o '0' // + y que estes seguro de la respuesta cual es || ejemplo de output: 1, 1, Emilio Ferrero; 2, 0, NA; 3, 0, NA;"
        })  
         pre_respuestas, conversation_history = process_openai_message(conversation_history)
         #print(conversation_history)
@@ -39,7 +39,9 @@ def handle_post_request():
         csv_data = StringIO(pre_respuestas.replace(";", "\n"))
         pre_respuestas = np.genfromtxt(csv_data, delimiter=",", dtype=str)
         user_message_count[telefono] = 0
-        if pre_respuestas[0, 1] == " No":
+
+        
+        if pre_respuestas[0, 2] == " NA":
             user_message_count[telefono] += 1
             pregunta_1 = "Muchas gracias por tomarte estos minutos para hacer la confirmación a la fiesta de Pupe!. ¿Podrías decirme el nombre y apellido de la persona que confirmás y si va a asistir al evento?"
             conversation_history.append({"role": "assistant", "content": pregunta_1})
@@ -52,10 +54,8 @@ def handle_post_request():
 
     if user_message_count[telefono] == 1:
         print("2")
-        if pre_respuestas[1, 1] == " No":
-            pregunta_2 = "Queremos que la fiesta se disfrute al máximo..."
-            conversation_history.append({"role": "assistant", "content": pregunta_2})
-            conversation_history.append({"role": "assistant", "content": "Podrias hacer un string con el siguiente formato, tomando el nombre sin el apellido del historial de charlas: Queremos que <Nombre> disfrute al máximo de la fiesta?"})
+        if pre_respuestas[1, 1] == " 0":
+            conversation_history.append({"role": "assistant", "content": "Queremos que <Nombre sin apellido> disfrute la fiesta al máximo, por eso nos gustaría saber si el invitado tiene alguna preferencia o restricción alimentaria (por ejemplo: celíaco, vegetariano, vegano, diabético). ¡Contanos y nos adaptamos!"})
             pregunta_2, conversation_history = process_openai_message(conversation_history)
             print(conversation_history)
             print(pre_respuestas)
@@ -70,7 +70,7 @@ def handle_post_request():
     if user_message_count[telefono] == 2:
         print("3")
 
-        if pre_respuestas[2, 1] == " No":
+        if pre_respuestas[2, 1] == " 0":
             pregunta_3 = "Muchas gracias por la info! ¿Necesitas confirmar asistencia de alguna persona más?"
             conversation_history.append({"role": "assistant", "content": pregunta_3})
             conversation_history.append({"role": "assistant", "content": "Podrias hacer un string afectuoso ycon el siguiente formato, tomando el nombre sin el apellido del historial de charlas: Además de <Nombre> ¿Necesitas confirmar asistencia de alguna persona más?"})
@@ -85,7 +85,9 @@ def handle_post_request():
 
     else:
         print("3")
-
+        conversation_history.append({"role": "assistant", "content":  "Podrías darme 1 mensaje pelado para ser enviado a un chatbot con este concepto <Gracias, cualquier duda llamala a Pau>"})
+        pregunta_4, conversation_history = process_openai_message(conversation_history)
+    
         return jsonify({
-            "pregunta": "Gracias! Ante cualquier duda llamala a Pau."
+            "pregunta": pregunta_4
         })
